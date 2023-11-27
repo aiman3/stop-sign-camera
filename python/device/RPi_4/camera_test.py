1cf7e7b3a4b657076589d4214ebc49aaad92645f
mario31274 <26250287+mario31274@users.noreply.github.com>
2023-11-27 18:12:44 -0500

    capture file (png)

--- python/device/RPi_4/camera_test.py
+++ python/device/RPi_4/camera_test.py
@@ -1,9 +1,10 @@
 from picamera2 import Picamera2, Preview
 import time
 
-camera = Picamera2()
-camera.start_preview(Preview.QT)
-preview_config = camera.create_preview_configuration()
-camera.configure(preview_config)
-camera.start()
+picam2 = Picamera2()
+picam2.start_preview(Preview.QT)
+preview_config = picam2.create_preview_configuration()
+picam2.configure(preview_config)
+picam2.start()
 time.sleep(5)
+picam2.capture_file("test.png")
