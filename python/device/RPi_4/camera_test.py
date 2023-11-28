62486ff0cff30db8fedb70572b85f15e991378d7
mario31274 <26250287+mario31274@users.noreply.github.com>
2023-11-28 03:29:39 -0500

    fix module not found

--- python/device/RPi_4/camera_test.py
+++ python/device/RPi_4/camera_test.py
@@ -3,7 +3,9 @@ from libcamera import controls
 from pathlib import Path
 import cv2
 import os
-from src.util import setup_temp_dir, TEMP_DIR
+import sys
+sys.path.append(str(Path(__file__).parent.parent.parent))
+from src.util import setup_temp_dir, TEMP_DIR  # noqa: E402
 
 
 def event_exit():
@@ -18,12 +20,9 @@ picam2 = Picamera2()
 preview_config = picam2.create_preview_configuration(
     main={
         "size": (1920, 1080),
-        "format": "BGR888"
-    },
-    lores={
-        "size": (1280, 720),
-        "format": "YUV420"
-    })
+        "format": "RGB888"
+    }
+)
 picam2.configure(preview_config)
 picam2.start()
 picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous})
@@ -31,8 +30,9 @@ picam2.capture_file(TEMP_DIR + "test_start.png")
 print('Camera Started Successfully!', 'Press q or Ctrl+C to abort')
 while True:
     try:
-        frame = picam2.capture_array("lores")
-        cv2.imshow("picamera2", frame)
+        frame = picam2.capture_array()
+        resized = cv2.resize(frame, (960, 540))
+        cv2.imshow("picamera2", resized)
         if cv2.waitKey(1) == ord('q'):
             event_exit()
             break
