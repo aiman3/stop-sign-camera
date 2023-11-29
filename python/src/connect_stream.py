dcc579040df33c749dd739fb297b42daca69834a
mario31274 <26250287+mario31274@users.noreply.github.com>
2023-11-29 08:22:22 -0500

    some sample scripts for streaming

---
+++ python/src/connect_stream.py
@@ -0,0 +1,17 @@
+# Usage: run "python stream.py" on RPi first, then run this script:
+#   python connect_stream.py
+
+import cv2
+
+cap = cv2.VideoCapture("tcp://raspberrypi.local:10001")
+while (True):
+    ret, frame = cap.read()
+    cv2.imshow('frame', frame)
+
+    # the 'q' button is set as the
+    # quitting button you may use any
+    # desired button of your choice
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
