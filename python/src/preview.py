28c415e5ac8444812947ef29e00c73ca00dea247
mario31274 <26250287+mario31274@users.noreply.github.com>
2023-11-29 21:37:42 -0500

    fix preview can't show more than one bbox

--- python/src/preview.py
+++ python/src/preview.py
@@ -2,7 +2,7 @@
 # Usage: python preview.py [car_name] [-h]
 # This script assumes following structure:
 #   stop-sign-camera/
-#   ├── dataset/
+#   ├── dataset_raw/
 #   │   └── license_plate/
 #   │       ├── annotations/
 #   │       │   ├── car_name.xml
@@ -28,25 +28,25 @@ args = parser.parse_args()
 
 car_name = args.car_name
 image_path = os.path.join(
-    ROOT_DIR, 'dataset', 'license_plate', 'images', car_name + '.png')
+    ROOT_DIR, 'dataset_raw', 'license_plate', 'images', car_name + '.png')
 annotation_path = os.path.join(
-    ROOT_DIR, 'dataset', 'license_plate', 'annotations',  car_name + '.xml')
+    ROOT_DIR, 'dataset_raw', 'license_plate', 'annotations',  car_name + '.xml')
 
 if os.path.exists(image_path):
     image = cv2.imread(image_path)
     tree = ET.parse(annotation_path)
     root = tree.getroot()
 
-    bbox = []
-    for b in root.find('./object/bndbox'):
-        bbox.append(int(b.text))
-
-    x1 = bbox[0]
-    y1 = bbox[1]
-    x2 = bbox[2]
-    y2 = bbox[3]
-
-    cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 4)
+    bboxs = []
+    for b in root.findall('./object/bndbox'):
+        bboxs.append(b)
+    for bbox in bboxs:
+        points = []
+        for b in bbox:
+            points.append(int(b.text))
+        x1, y1 = points[0], points[1]
+        x2, y2 = points[2], points[3]
+        cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 4)
 
     cv2.imshow(car_name + '.png', image)
     cv2.waitKey(0)
@@ -56,7 +56,7 @@ else:
     print('Error: "'+car_name+'.png" not found')
     print('This script assumes following structure:\n\
     stop-sign-camera/\n\
-    ├── dataset/\n\
+    ├── dataset_raw/\n\
     │   └── license_plate/\n\
     │       ├── annotations/\n\
     │       │   ├── car_name.xml\n\
